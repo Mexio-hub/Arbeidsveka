eed86116d6acd5d422638441f05bbab25bfa243f
Henrik <johndoe@example.com>
2022-12-09 11:28:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let array = [
+    {"Game": "Valorant", "Film": "Titanic", "Serie": "Grey's Anatomy", "Artist": "Adele", "Bøker": "Nevermoor"},
+    {"Game": "Minecraft", "Film": "Karate Kid", "Serie": "Lost In Space", "Artist": "Lewis Capaldi", "Bøker": "Percy Jackson"},
+    {"Game": "MFS2020", "Film": "Titanic", "Serie": "Heart Stopper", "Artist": "Benson Boone", "Bøker": "Heart Stopper"},
+    {"Game": "CS:GO", "Film": "Fast and Furious", "Serie": "Young Royals", "Artist": "Tom Odell", "Bøker": "Harry Potter"},
+    {"Gane": "FS2019", "Film": "Skjelvet", "Serie": "Wednesday", "Artist": "Mariah Carey", "Bøker": "Full Circle"}];
